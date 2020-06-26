ed8a9786bf9932327fc98734bbebd1ac6a8d77b4
matt <bronx0807@gmail.com>
2020-06-26 08:42:45 +0800

    MessageSource args demo

--- src/main/java/com/abc/demo/DemoApplication.java
+++ src/main/java/com/abc/demo/DemoApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 public class DemoApplication {
